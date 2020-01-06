0e375b28af998bdd78c7ba254a6ad34755366544
Javiercerna <javier.cerna@pucp.pe>
2020-01-06 12:59:58 +0100

    Implement particle filter (slow & not sure about 'estimate')

--- particle-filter/particle_filter.py
+++ particle-filter/particle_filter.py
@@ -24,26 +24,12 @@ class ParticleFilter():
 
     def update_particle_weights(self, measurement):
         for ind in range(len(self.particles)):
-            # matrix_likelihood = scipy.stats.norm(
-            #    self.H @ self.particles[ind], self.R).pdf(measurement)
-            # matrix_likelihood = np.nan_to_num(matrix_likelihood)
+            matrix_likelihood = scipy.stats.norm(
+                self.H @ self.particles[ind],
+                np.sqrt(self.R)).pdf(measurement)
+            matrix_likelihood = np.nan_to_num(matrix_likelihood)
 
-            # print('Matrix likelihood:', matrix_likelihood)
-            x = self.H @ self.particles[ind]
-            l1 = scipy.stats.norm(
-                x[0],
-                math.sqrt(self.R[0][0])).pdf(
-                measurement[0])
-            l2 = scipy.stats.norm(
-                x[1],
-                math.sqrt(self.R[1][1])).pdf(
-                measurement[1])
-            # print('Separate likelihood:', l1, l2)
-
-            #assert abs(matrix_likelihood[0][0] - l1) < 1e-10
-            #assert abs(matrix_likelihood[1][1] - l2) < 1e-10
-
-            self.particles_weights[ind] = math.sqrt(l1 ** 2 + l2 ** 2)
+            self.particles_weights[ind] = sum(sum(matrix_likelihood))
             self.particles_weights[ind] += 1e-300  # Avoid round-off to 0
 
         sum_weights = sum(self.particles_weights)
@@ -51,15 +37,10 @@ class ParticleFilter():
                                   for weight in self.particles_weights]
 
     def resample(self):
-        # Calculate cumulative distribution function (cdf)
-        cdf = [0] * len(self.particles_weights)
-        for ind in range(len(cdf)):
-            cdf[ind] = sum(self.particles_weights[:ind+1])
+        cdf = self._calculate_cdf()
 
-        # print(cdf)
-
-        resampled_particles = [None] * len(self.particles)
         # Multinomial resample
+        resampled_particles = [None] * len(self.particles)
         for ind in range(len(self.particles)):
             rand_num = np.random.rand()
             filtered_indexes = [
@@ -78,23 +59,8 @@ class ParticleFilter():
             max(self.particles_weights))
         return self.particles[max_weight_ind]
 
-    def calculate_mean(self):
-        mean_particle = np.array([0.0, 0.0, 0.0, 0.0])
-
-        for particle in self.particles:
-            mean_particle += particle
-
-        return mean_particle / len(self.particles)
-
     def _create_particle(self, x0):
-        #x = np.random.uniform(-1, 1)
-        x = np.random.normal(x0[0], math.sqrt(self.Q[0][0]))
-        vx = x0[1]
-        #y = np.random.uniform(-1, 1)
-        y = np.random.normal(x0[2], math.sqrt(self.Q[2][2]))
-        vy = x0[3]
-
-        return np.array([x, vx, y, vy])
+        return np.random.multivariate_normal(mean=x0, cov=self.Q)
 
     def _propagate_particle(self, particle):
         zero_mean = np.zeros(self.Q.shape[0])
@@ -103,3 +69,11 @@ class ParticleFilter():
             np.random.multivariate_normal(mean=zero_mean, cov=self.Q)
 
         return particle
+
+    def _calculate_cdf(self):
+        cdf = [0] * len(self.particles_weights)
+
+        for ind in range(len(cdf)):
+            cdf[ind] = sum(self.particles_weights[:ind+1])
+
+        return cdf
--- particle-filter/run_filter.py
+++ particle-filter/run_filter.py
@@ -1,3 +1,11 @@
+"""
+Based on the work of Marko Cotra.
+
+References: 
+* https://gist.github.com/cotramarko
+* https://towardsdatascience.com/wtf-is-sensor-fusion-part-2-the-good-old-kalman-filter-3642f321440
+
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,8 +14,8 @@ from models import simulate_system, create_model_parameters
 
 np.random.seed(0)
 (A, H, Q, R) = create_model_parameters()
-simulation_time = 10
-n_particles = 200
+simulation_time = 20
+n_particles = 1000
 
 # Initial state
 x0 = np.array([0, 0.1, 0, 0.1])
@@ -21,20 +29,11 @@ for k in range(simulation_time):
     particle_filter.predict()
     particle_filter.update_particle_weights(measurements[k, :])
 
-    if k == simulation_time - 1:
-        particles_x = [particle[0] for particle in particle_filter.particles]
-        particles_y = [particle[2] for particle in particle_filter.particles]
-
-        plt.figure()
-        plt.scatter(particles_x, particles_y,
-                    c=particle_filter.particles_weights, s=30)
-        plt.colorbar()
-
     estimated_state[k, :] = particle_filter.calculate_estimate()
 
     particle_filter.resample()
 
-
+# Plot X-Y
 plt.figure()
 plt.plot(state[:, 0], state[:, 2], '-bo')
 plt.plot(estimated_state[:, 0], estimated_state[:, 2], '-ko')
@@ -43,6 +42,7 @@ plt.xlabel('x [m]')
 plt.ylabel('y [m]')
 plt.legend(['true state', 'inferred state', 'observed measurement'])
 
+# Plot particles
 particles_x = [particle[0] for particle in particle_filter.particles]
 particles_y = [particle[2] for particle in particle_filter.particles]
 
